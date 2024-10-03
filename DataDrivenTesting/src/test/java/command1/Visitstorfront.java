5d72fb7dc54abf37fb0794c9ed642164744bc985
kiran <kapatel9191@gmail.com>
2024-10-03 21:21:22 +0530

    created new java file to visti front stor

---
+++ DataDrivenTesting/src/test/java/command1/Visitstorfront.java
@@ -0,0 +1,24 @@
+package command1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Visitstorfront {
+
+	public static void main(String[] args) {
+		System.setProperty("wedriver.gecko.driver", "C:/Software-selenium.exe");
+		WebDriver  driver = new FirefoxDriver();
+		driver.get("https://www.opencart.com/index.php?route=cms/demo");
+		
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl()); 
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
